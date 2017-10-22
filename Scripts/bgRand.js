8b5c21b4d441b164093ab00372c1f3637f6ad1ff
John Townsend <john.townsend@trojans.dsu.edu>
2017-10-22 15:59:34 -0500

    Fixed browser compatibilty issues and a few glitches

--- Scripts/bgRand.js
+++ Scripts/bgRand.js
@@ -1,9 +1,9 @@
-let colors = ["rgb(149,0,211)", "rgb(75,0,130)", "rgb(0,0,255)", "rgb(0,255,0)", "rgb(255,255,0)", "rgb(255,127,0)", "rgb(255,0,0)"];
+var colors = ["rgb(149,0,211)", "rgb(75,0,130)", "rgb(0,0,255)", "rgb(0,255,0)", "rgb(255,255,0)", "rgb(255,127,0)", "rgb(255,0,0)"];
 
 
 $(document).ready(function(){
-    let cont = $("#container");
+    var cont = $("#container");
     window.setInterval(function(){
         cont.css("background-color", colors[Math.floor(Math.random() * colors.length)]);
-    }, 500)
+    }, 1000);
 });
--- Scripts/grav.js
+++ Scripts/grav.js
@@ -1,11 +1,10 @@
 
 var grabBox = $("#grabBox");
 
+var maxWidth, maxHeight, minWidth, minHeight;
 
-var maxWidth = $(window).width() * .9;
-var maxHeight = $(window).height() * .8;
-var minWidth = $(window).width() - maxWidth;
-var minHeight = $(window).height() - maxHeight;
+var topOffset = $(window).height();
+
 
 var glideTime = 700;
 var glideSteps = 50;
@@ -18,15 +17,22 @@ var box = {
     y : minHeight,
     prevX : [0,0,0,0,0,0,0,0,0,0],
     prevY : [0,0,0,0,0,0,0,0,0,0],
-    width : 25,
-    height: 25,
+    width : 50,
+    height: 50,
     clicked: false,
     freefall: 0
-}
+};
 
 $(document).ready(function(){
-
+    var cont = $("#container");
+    
+    maxWidth = parseInt(cont.css("width"));
+    maxHeight = parseInt(cont.css("height"));
+    minWidth = cont.offset().left;
+    minHeight = cont.offset().top ;
+    
     grabBox = $("#grabBox");
+    console.log(maxWidth + " " + maxHeight);
     
     grabBox.on("mousedown", boxClick);
     
@@ -57,37 +63,41 @@ function boxClick(){
 function boxDrag(event){
     box.prevX.push(box.x);
     box.prevY.push(box.y);
-    box.x = event.pageX;
-    box.y = event.pageY;
+    box.x = event.pageX - box.width / 2;
+    box.y = event.pageY - box.height / 2;
 }
 
 function updateBox(){
-    console.log(`updating ${box.x} ${box.y}`);
-    if(box.x > maxWidth)
-        box.x = maxWidth;
+    console.log(`updating ${box.x} ${box.y} max:${maxWidth} ${maxHeight} min:${minWidth} ${minHeight}`);
+    if(box.x > maxWidth - box.width)
+        box.x = maxWidth - box.width;
     else if(box.x < minWidth)
         box.x = minWidth;
     
-    if(box.y > maxHeight)
-        box.y = maxHeight;
-    else if(box.y < minHeight)
+    if(box.y > maxHeight - box.height)
+        box.y = maxHeight - box.height;
+    else if(box.y < minHeight){
         box.y = minHeight;
+        console.log(`setting box.y to ${box.y} == ${minHeight}`)
+    }
 
-    grabBox.css("left", box.x - 12.5);
-    grabBox.css("top", box.y - 12.5);
+    grabBox.css("left", box.x);
+    grabBox.css("top", box.y);
+    grabBox.css("height", box.height + "px");
+    grabBox.css("width", box.width + "px");
 }
 
 function gravityBox(){
 
-    let xSpeed = box.x - box.prevX.pop();
-    let xStep = xSpeed / stepTime;
-    let ySpeed = box.y - box.prevY.pop();
-    let yStep = ySpeed / stepTime;
+    var xSpeed = box.x - box.prevX.pop();
+    var xStep = xSpeed / stepTime;
+    var ySpeed = box.y - box.prevY.pop();
+    var yStep = ySpeed / stepTime;
 
     var gravityInterval = setInterval(function(){
-            if(!box.clicked && box.y < maxHeight){
-                box.x = Math.abs((box.x + xSpeed));
-                box.y = Math.abs((box.y + ySpeed));
+            if(!box.clicked && box.y < maxHeight - box.height){
+                box.x = (box.x + xSpeed);
+                box.y = (box.y + ySpeed);
                 
                 box.prevX.push(box.x);
                 box.prevY.push(box.y);
